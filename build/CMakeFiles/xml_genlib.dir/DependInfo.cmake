6e97a47ebdfc92d865d1174252459e50520cf992
trocolit2 <trocolit2@gmail.com>
2016-02-24 09:54:44 -0300

    adjust code to run on eclipse IDE

---
+++ build/CMakeFiles/xml_genlib.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tiagotrocoli/Documents/myCode/apriltags_sdf_generator/root/libraries/xml_gen.cpp" "/home/tiagotrocoli/Documents/myCode/apriltags_sdf_generator/build/CMakeFiles/xml_genlib.dir/libraries/xml_gen.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/home/tiagotrocoli/Documents/myCode/apriltags_sdf_generator/root/./libraries"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/xml_genlib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+FILE(REMOVE_RECURSE
+  "libxml_genlib.a"
+)
